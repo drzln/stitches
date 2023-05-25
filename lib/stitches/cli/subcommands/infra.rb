17f55289af335673de24641c436b77350ec4aaa0
luis <ldesiqueira@pinger.com>
2023-05-24 23:29:02 -0300

    errors

--- lib/stitches/cli/subcommands/infra.rb
+++ lib/stitches/cli/subcommands/infra.rb
@@ -1,10 +1,18 @@
 require_relative %(./stitches)
+require_relative %(../../errors/no_infra_target_error)
+require_relative %(../../errors/incorrect_subcommand_error)
 
 class InfraCommand < StitchesCommand
+  @name = :infra
+
   usage do
     desc %(manage infrastructure)
     program %(stitches)
     command %(infra)
+  end
+
+  argument :subcommand do
+    desc %(the subcommand)
   end
 
   argument :target do
@@ -14,9 +22,19 @@ class InfraCommand < StitchesCommand
   def run(argv)
     parse(argv)
 
+    puts params
+    puts params.keys
+
+    raise IncorrectSubcommandError unless correct_subcommand?(params[:subcommand])
     raise NoInfraTargetError unless params[:target]
 
     print help
     exit
+  end
+
+  private
+
+  def correct_subcommand?(sbcmd)
+    @name.to_s.eql?(sbcmd.to_s)
   end
 end
