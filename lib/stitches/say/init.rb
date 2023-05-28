bc2f5fa54c5a8881f71d18e792d064e20696e571
luis <ldesiqueira@pinger.com>
2023-05-28 12:21:59 -0300

    init a small say module

--- lib/stitches/say/init.rb
+++ lib/stitches/say/init.rb
@@ -1,2 +1,20 @@
+require %(tty-box)
+
 module Say
+  class << self
+    def terminal(msg)
+      box = TTY::Box.frame width: msg.length + 4,
+                           align: :center,
+                           border: :thick,
+                           padding: 1,
+                           height: 5,
+                           style: {
+                             fg: :black,
+                             bg: :white
+                           }
+      puts box + "\n"
+      puts "  #{msg}  "
+      puts "\n" + box
+    end
+  end
 end
