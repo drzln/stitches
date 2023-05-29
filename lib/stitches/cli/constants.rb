25dca765eb0895e943f486a6c6ef2acb6be983db
luis <ldesiqueira@pinger.com>
2023-05-29 10:06:42 -0300

    add cache dir to constants

--- lib/stitches/cli/constants.rb
+++ lib/stitches/cli/constants.rb
@@ -8,11 +8,17 @@ module Constants
 
   # project related constants
   PROJECT_VERSION = %i[version.rb].freeze
+
+  # order of elements matters here
+  # projects should be processed
+  # by collecting ruby files in exactly
+  # this order. changing this can significantly
+  # impact how a project is processed.
   PROJECT_SRC_DIRS = %i[
     lib
     pre
-    post
     resources
+    post
   ].freeze
 
   # configuration extensions
